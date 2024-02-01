dbdff4641c00865050f24979745a2ff39229b137
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-02 04:36:59 +0530

    initialize values on start btn click, condititon for newCard(), player obj for playerEl

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Guest",
+    chips: 100,
+    // sayHello: function(){
+    //     console.log("Heisann!")
+    // } this is a method -> Function attached to object calling syntax ---> player.sayHello()
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")//document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1; // Generates a random number between 1 and 13
@@ -44,13 +52,20 @@ function renderGame(){
 }
 
 function newCard(){
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if(isAlive && !hasBlackJack){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
 }
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
