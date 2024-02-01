2c4f80ab70f2f72c7102307887486a7ca65fd342
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-01 12:11:08 +0530

    startGame function created

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if(sum<=20){
-    console.log("Do you want to draw a new card? 🙂")
-} else if(sum === 21){
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else{
-    console.log("You're out of the game! 😭")
-    isAlive = false
-    message = "we're logging out"
+function startGame(){
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)
 }
-
-console.log(message)
