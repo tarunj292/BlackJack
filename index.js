28659ce1008c6cbbef44fdbe662cdb01ac93130f
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-01 14:26:54 +0530

    created cards[], getRandomCard();added forloop for cardsEl

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10
-let secondCard = 2
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -8,8 +9,26 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")//document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1; // Generates a random number between 1 and 13
+    switch (randomNumber) {
+        case 1:
+            return 11; // Ace
+        case 11:
+        case 12:
+        case 13:
+            return 10; // Jack, Queen, King
+        default:
+            return randomNumber;
+    }
+}
+
+
 function renderGame(){
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: "
+    for(let i = 0; i<cards.length; i++){
+        cardsEl.textContent += (cards[i] + " ")
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -25,10 +44,10 @@ function renderGame(){
 }
 
 function newCard(){
-    console.log("Drawing a new card from the deck!")
-    let newCard = 3
-    console.log(sum)
-    sum = sum + newCard
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
 
